9f35273e10de887dd239fbf4fd06abcb714e8be1
abhiraj2912 <abhirajaravind408@gmail.com>
2022-12-08 21:08:47 +0530

    first commit

---
+++ src/test/java/com/example/libraryappnewbackend/LibraryappnewBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.libraryappnewbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryappnewBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
